a353a85fd145dbeec70b95d76270582ef39080ee
Francois Michon <francois.mchn@gmail.com>
2021-01-25 00:46:27 +0100

    typo

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,18 @@ module.exports = {
           },
           'img-loader'
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   },
